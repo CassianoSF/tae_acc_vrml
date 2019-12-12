6b81a55e7798ccc94edb6b9b01781a72683de64c
CassianoSF <cassianosfranco@gmail.com>
2019-12-12 17:59:54 -0300

    UNDO

--- obj-wrl.py
+++ obj-wrl.py
@@ -75,7 +75,6 @@ class Parser:
             self.arquivoWRL.write("                normal Normal {\n")
             self.arquivoWRL.write("                  vector [\n")
         self.state = next_state
-        stejbsapdifbsdapifbuh
 
     def start(self):
         self.arquivoWRL.write("#VRML V2.0 utf8\n")
